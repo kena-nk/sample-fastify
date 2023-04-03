a27df4fbbb78d82db2726be314e26e60916288f1
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2023-04-03 19:48:05 +0900

    [update]curlコマンドを実行してデータ追加できるようになった
    curl -X POST -H "Content-Type: application/json"  -d '{"animal":"hoge"}'  http://localhost:3000/animals

--- our-first-route.js
+++ our-first-route.js
@@ -17,6 +17,32 @@ async function routes(fastify, options) {
     }
     return result;
   });
+
+  fastify.get('/animals/:animal', async (request, reply) => {
+    const result = await collection.findOne({ animal: request.params.animal });
+    if (!result) {
+      throw new Error('Invalid value');
+    }
+    return result;
+  });
+
+  const animalBodyJsonSchema = {
+    type: 'object',
+    required: ['animal'],
+    properties: {
+      animal: { type: 'string' },
+    },
+  };
+
+  const schema = {
+    body: animalBodyJsonSchema,
+  };
+
+  fastify.post('/animals', { schema }, async (request, reply) => {
+    // we can use the `request.body` object to get the data sent by the client
+    const result = await collection.insertOne({ animal: request.body.animal });
+    return result;
+  });
 }
 
 export default routes;
